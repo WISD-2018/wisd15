700b73028a22c4154f086f148ca78e8648ba72d8
江明宸 <quzz96er43@mail.com>
2018-12-05 17:03:23 +0800

    dd.d1

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,16 @@ Route::get('/aa/a1',function(){
 });
 
 Route::get('/aa/a2',function(){
-   return view('aa.a2'); 
-
+   return view('aa.a2');
+});
 Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
 
 Route::get('/bb/b2', function () {
     return view('bb.b2');
+});
+
+Route::get('/dd/d1', function () {
+    return view('dd.d1');
 });
